dca5de17a99839582e6dbe6b22af8ff372a3ebc0
kemboi22 <kemboielvis22@gmail.xom>
2023-08-19 18:42:54 +0300

    Done with Welcome View and Loader View

--- ReactApp/tailwind.config.js
+++ ReactApp/tailwind.config.js
@@ -1,12 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    darkMode: "media", // or 'media' or 'class'
+    theme: {
+        fontFamily: {
+            display: ["Open Sans", "sans-serif"],
+            body: ["Open Sans", "sans-serif"],
+        },
+        extend: {
+            screens: {
+                "mf": "990px",
+            },
+            keyframes: {
+                "slide-in": {
+                    "0%": {
+                        "-webkit-transform": "translateX(120%)",
+                        transform: "translateX(120%)",
+                    },
+                    "100%": {
+                        "-webkit-transform": "translateX(0%)",
+                        transform: "translateX(0%)",
+                    },
+                },
+            },
+            animation: {
+                "slide-in": "slide-in 0.5s ease-out",
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    // eslint-disable-next-line no-undef
+    plugins: [require("@tailwindcss/forms")],
 }
 
